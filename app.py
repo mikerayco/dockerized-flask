39cc6106cc144d21e605b09599e3fbdd98192bca
Mike Rayco <mikerayco@hey.com>
2021-03-24 22:18:59 +0800

    added function for a challenge

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import request
+
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello! This is a Flask app running in a container!'
+    return 'Hello! This is a Flask app running in a container!', 200
+
+# #complete the function below
+# def time():
+#     tz = request.json['timezone']
+#     return result ,200
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
