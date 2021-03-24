ab4e1419ce7ab6ec28a447b106ae22c74a4d05c5
Mike Rayco <mikerayco@hey.com>
2021-03-24 21:55:29 +0800

    added http method to function

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     return 'Hello! This is a Flask app running in a container!'
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
