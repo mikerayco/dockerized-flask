698fa11771d90de9fc65eab3527235de833cbf18
Mike Rayco <michaelangelorayco@gmail.com>
2022-02-23 09:58:17 +0800

    added aloha endpoint

--- app.py
+++ app.py
@@ -4,22 +4,15 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def hello():
-    return 'Hello! This is a Flask app running in a container!', 200
-
-@app.route('/yow', methods=['GET'])
-def yow():
-    return 'Yow wassup man!', 200
+    return "Hello! This is a Flask app running in a container!", 200
 
 
-
-# #complete the function below
-#@app.route('/time', methods=['POST'])
-# def time():
-#     tz = request.json['timezone']
-#     return result ,200
+@app.route("/aloha", methods=["GET"])
+def aloha():
+    return "Aloha World!", 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
